58c7b4b2d8de0e61b29ef48346c0e08d75cc2146
Pavithra Y M <pavithraym55@gmail.com>
2025-11-26 10:55:37 +0530

    Fix footer layout and update services

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './MyWork.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import mywork_data from '../../assets/mywork_data.js'
-import arrow_icon from '../../assets/arrow_icon.svg'
+// import arrow_icon from '../../assets/arrow_icon.svg'
 const MyWork = () => {
   return (
     <div id='work' className='mywork'>
@@ -15,10 +15,10 @@ const MyWork = () => {
                return <img key={index} src={work.w_img} alt="" />
             })}
         </div>
-        <div className="mywork-showmore">
+        {/* <div className="mywork-showmore">
             <p>Show more </p>
             <img src= {arrow_icon} alt="" />
-        </div>
+        </div> */}
         </div>
   )
 }
